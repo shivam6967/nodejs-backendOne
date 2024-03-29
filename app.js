fb3603221c347d65c945f38f5ac1c1a561976db9
shivammathapati <shivam.mathapati@walchandsangli.ac.in>
2024-03-29 19:34:25 +0530

    again modifining the app.js file

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
 import express from "express";
-import userRouter from "./routes/user.routes.js";
-import taskRouter from "./routes/task.routes.js";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
-import { newTask } from "./controllers/task.controllers.js";
-import { errorMiddleware } from "./middlewares/error.middlewares.js";
 import cors from "cors";
 
+// Importing routes and controllers
+import userRouter from "./routes/user.routes.js";
+import taskRouter from "./routes/task.routes.js";
+import { errorMiddleware } from "./middlewares/error.middlewares.js";
 
 export const app = express();
 
-// Configuring the dotenv file : 
+// Configuring the dotenv file
 config({
     path: "./data/config.env"
-})
-
-// Middleware : for sending json data from the POSTMAN 
-app.use( "/api/v1/users" , userRouter);
-app.use( "/api/v1/task" , taskRouter);
-app.use(express.json());
-app.use(cookieParser());
-app.use(errorMiddleware);
-app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET" , "POST" , "PUT" , "DELETE"], 
-    credentials: true,
-}))
-
-
-app.get("/" , (req, res) => {
-    res.send("Server is Working");
 });
 
+// Middleware
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
+app.use(express.json());
+app.use(cookieParser());
 
+// Routes
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
+
+// Error handling middleware
+app.use(errorMiddleware);
+
+// Default route
+app.get("/", (req, res) => {
+    res.send("Server is Working");
+});
